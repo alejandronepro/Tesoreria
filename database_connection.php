3e684ffc46a89212246745d53ec4ee6bece9640e
Alejandro Valdez <alejandronepro@gmail.com>
2018-03-21 13:23:44 -0400

    configured database connection

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,17 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
+// $connect = new PDO("mysql:host=localhost;dbname=iglesie5_tesoreria", "iglesie5_admin", "ICASD5241");
+// $conn = mysqli_connect("localhost", "iglesie5_avaldez", "SobreTodo7?", "iglesie5_subscribers");  
 
+try {
+  $connect = new PDO("mysql:host=localhost;dbname=iglesie5_tesoreria", "iglesie5_admin", "ICASD5241");
+  // set the PDO error mode to exception
+  $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo "Connected successfully"; 
+  }
+catch(PDOException $e)
+  {
+  echo "Connection failed: " . $e->getMessage();
+  }
 ?>
