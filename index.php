d1e9b1ef2d607e6cf893952be3d8f0f6afeb7cc1
Alejandro Valdez <alejandronepro@gmail.com>
2018-03-22 17:33:22 -0400

    Added new files

--- index.php
+++ index.php
@@ -1,200 +1,155 @@
-
-<!--index.php !-->
-
-<html>
-	<head>
-		<title>Ajax Crud on Dynamically Add Remove Input Fields in PHP</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<br />
-			<br />
-			<h2 align="center">Ajax Crud on Dynamically Add Remove Input Fields in PHP</h2><br />
-			<div align="right">
-				<button type="button" name="add" id="add" class="btn btn-info">Add</button>
-			</div>
-			<br />
-			<div id="result"></div>
-		</div>
-	</body>
-</html>
-
-<div id="dynamic_field_modal" class="modal fade" role="dialog">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form method="post" id="add_name">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">Add Details</h4>
-				</div>
-				<div class="modal-body">
-					<div class="form-group">
-		      			<input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" />
-		      		</div>
-		      		<div class="table-responsive">
-		      			<table class="table" id="dynamic_field">
-
-		      			</table>
-		      		</div>
-				</div>
-				<div class="modal-footer">
-					<input type="hidden" name="hidden_id" id="hidden_id" />
-					<input type="hidden" name="action" id="action" value="insert" />
-					<input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
-				</div>
-			</form>
-		</div>
-	</div>
-
-</div>
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PHP CRUD</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style type="text/css">
+table tr th, table tr td{font-size: 1.2rem;}
+.row{ margin:20px 20px 20px 20px;width: 100%;}
+.glyphicon{font-size: 20px;}
+.glyphicon-plus{float: right;}
+a.glyphicon{text-decoration: none;}
+a.glyphicon-trash{margin-left: 10px;}
+.none{display: none;}
+</style>
 <script>
-$(document).ready(function(){
-
-	load_data();
-
-	var count = 1;
-
-	function load_data()
-	{
-		$.ajax({
-			url:"fetch.php",
-			method:"POST",
-			success:function(data)
-			{
-				$('#result').html(data);
-			}
-		})
-	}
-
-	function add_dynamic_input_field(count)
-	{
-		var button = '';
-		if(count > 1)
-		{
-			button = '<button type="button" name="remove" id="'+count+'" class="btn btn-danger btn-xs remove">x</button>';
-		}
-		else
-		{
-			button = '<button type="button" name="add_more" id="add_more" class="btn btn-success btn-xs">+</button>';
-		}
-		output = '<tr id="row'+count+'">';
-		output += '<td><input type="text" name="programming_languages[]" placeholder="Add Programming Languages" class="form-control name_list" /></td>';
-		output += '<td align="center">'+button+'</td></tr>';
-		$('#dynamic_field').append(output);
-	}
-
-	$('#add').click(function(){
-		$('#dynamic_field').html('');
-		add_dynamic_input_field(1);
-		$('.modal-title').text('Add Details');
-		$('#action').val("insert");
-		$('#submit').val('Submit');
-		$('#add_name')[0].reset();
-		$('#dynamic_field_modal').modal('show');
-	});
-
-	$(document).on('click', '#add_more', function(){
-		count = count + 1;
-		add_dynamic_input_field(count);
-	});
-
-	$(document).on('click', '.remove', function(){
-		var row_id = $(this).attr("id");
-		$('#row'+row_id).remove();
-	});
-
-	$('#add_name').on('submit', function(event){
-		event.preventDefault();
-		if($('#name').val() == '')
-		{
-			alert("Enter Your Name");
-			return false;
-		}
-		var total_languages = 0;
-		$('.name_list').each(function(){
-			if($(this).val() != '')
-			{
-				total_languages = total_languages + 1;
-			}
-		});
-
-		if(total_languages > 0)
-		{
-			var form_data = $(this).serialize();
-
-			var action = $('#action').val();
-			$.ajax({
-				url:"action.php",
-				method:"POST",
-				data:form_data,
-				success:function(data)
-				{
-					if(action == 'insert')
-					{
-						alert("Data Inserted");
-					}
-					if(action == 'edit')
-					{
-						alert("Data Edited");
-					}
-					add_dynamic_input_field(1);
-					load_data();
-					$('#add_name')[0].reset();
-					$('#dynamic_field_modal').modal('hide');
-				}
-			});
-		}
-		else
-		{
-			alert("Please Enter at least one programming languages");
-		}
-	});
-
-	$(document).on('click', '.edit', function(){
-		var id = $(this).attr("id");
-		$.ajax({
-			url:"select.php",
-			method:"POST",
-			data:{id:id},
-			dataType:"JSON",
-			success:function(data)
-			{
-				$('#name').val(data.name);
-				$('#dynamic_field').html(data.programming_languages);
-				$('#action').val('edit');
-				$('.modal-title').text("Edit Details");
-				$('#submit').val("Edit");
-				$('#hidden_id').val(id);
-				$('#dynamic_field_modal').modal('show');
-			}
-		});
-	});
-
-	$(document).on('click', '.delete', function(){
-		var id = $(this).attr("id");
-		if(confirm("Are you sure want to remove this data?"))
-		{
-			$.ajax({
-				url:"delete.php",
-				method:"POST",
-				data:{id:id},
-				success:function(data)
-				{
-					load_data();
-					alert("Data removed");
-				}
-			})
-		}
-	});
-
-});
+function getUsers(){
+    $.ajax({
+        type: 'POST',
+        url: 'userAction.php',
+        data: 'action_type=view&'+$("#userForm").serialize(),
+        success:function(html){
+            $('#userData').html(html);
+        }
+    });
+}
+function userAction(type,id){
+    id = (typeof id == "undefined")?'':id;
+    var statusArr = {add:"added",edit:"updated",delete:"deleted"};
+    var userData = '';
+    if (type == 'add') {
+        userData = $("#addForm").find('.form').serialize()+'&action_type='+type+'&id='+id;
+    }else if (type == 'edit'){
+        userData = $("#editForm").find('.form').serialize()+'&action_type='+type;
+    }else{
+        userData = 'action_type='+type+'&id='+id;
+    }
+    $.ajax({
+        type: 'POST',
+        url: 'userAction.php',
+        data: userData,
+        success:function(msg){
+            if(msg == 'ok'){
+                alert('User data has been '+statusArr[type]+' successfully.');
+                getUsers();
+                $('.form')[0].reset();
+                $('.formData').slideUp();
+            }else{
+                alert('Some problem occurred, please try again.');
+            }
+        }
+    });
+}
+function editUser(id){
+    $.ajax({
+        type: 'POST',
+        dataType:'JSON',
+        url: 'userAction.php',
+        data: 'action_type=data&id='+id,
+        success:function(data){
+            $('#idEdit').val(data.id);
+            $('#nameEdit').val(data.name);
+            $('#emailEdit').val(data.email);
+            $('#phoneEdit').val(data.phone);
+            $('#editForm').slideDown();
+        }
+    });
+}
 </script>
-
-
-
-
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="panel panel-default users-content">
+            <div class="panel-heading">Users <a href="javascript:void(0);" class="glyphicon glyphicon-plus" id="addLink" onclick="javascript:$('#addForm').slideToggle();"></a></div>
+            <div class="panel-body  formData" id="addForm">
+                <!-- <h2 id="actionLabel">Add User</h2> -->
+                <form class="form" id="userForm">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" id="name"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" name="email" id="email"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone</label>
+                        <input type="text" class="form-control" name="phone" id="phone"/>
+                    </div>
+                    <a href="javascript:void(0);" class="btn btn-danger" onclick="$('#userForm')[0].reset()">Reset</a>
+                    <a href="javascript:void(0);" class="btn btn-warning" onclick="$('#addForm').slideUp()">Hide</a>
+                    <a href="javascript:void(0);" class="btn btn-success" onclick="userAction('add')">Add</a>
+                </form>
+            </div>
+            <div class="panel-body none formData" id="editForm">
+                <h2 id="actionLabel">Edit User</h2>
+                <form class="form" id="userForm">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" id="nameEdit"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" name="email" id="emailEdit"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone</label>
+                        <input type="text" class="form-control" name="phone" id="phoneEdit"/>
+                    </div>
+                    <input type="hidden" class="form-control" name="id" id="idEdit"/>
+                    <a href="javascript:void(0);" class="btn btn-warning" onclick="$('#editForm').slideUp();">Cancel</a>
+                    <a href="javascript:void(0);" class="btn btn-success" onclick="userAction('edit')">Update User</a>
+                </form>
+            </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody id="userData">
+                    <?php
+                        include 'DB.php';
+                        $db = new DB();
+                        $users = $db->getRows('users',array('order_by'=>'id DESC'));
+                        if(!empty($users)): $count = 0; foreach($users as $user): $count++;
+                    ?>
+                    <tr>
+                        <td><?php echo '#'.$count; ?></td>
+                        <td><?php echo $user['name']; ?></td>
+                        <td><?php echo $user['email']; ?></td>
+                        <td><?php echo $user['phone']; ?></td>
+                        <td>
+                            <a href="javascript:void(0);" class="glyphicon glyphicon-edit" onclick="editUser('<?php echo $user['id']; ?>')"></a>
+                            <a href="javascript:void(0);" class="glyphicon glyphicon-trash" onclick="return confirm('Are you sure to delete data?')?userAction('delete','<?php echo $user['id']; ?>'):false;"></a>
+                        </td>
+                    </tr>
+                    <?php endforeach; else: ?>
+                    <tr><td colspan="5">No user(s) found......</td></tr>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+</html>
